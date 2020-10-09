a6005ea17979877b4b109cc55d414b5f65ad0309
Ali Rashid <ali.rashed@live.com>
2020-10-09 00:18:40 -0700

    edited server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
 const path = require("path");
+const cors = require("cors");
 
 //Connect Database
 connectDB();
